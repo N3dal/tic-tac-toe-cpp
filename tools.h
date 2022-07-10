d5a649b68fbeabb2f3d186f79fef114c58f924e5
N3dal <nedalxzo@gmail.com>
2022-07-10 13:50:41 +0200

    create tools header file and create get_os_name function

---
+++ tools.h
@@ -0,0 +1,21 @@
+std::string get_os_name()
+{
+    /*return os name*/
+
+
+    #ifdef _WIN32
+    return "Windows 32-bit";
+    #elif _WIN64
+    return "Windows 64-bit";
+    #elif __APPLE__ || __MACH__
+    return "Mac OSX";
+    #elif __linux__
+    return "Linux";
+    #elif __FreeBSD__
+    return "FreeBSD";
+    #elif __unix || __unix__
+    return "Unix";
+    #else
+    return "Other";
+    #endif
+} 
