f2eb24c03b3af9ac7a743d2eca6b391a3a968794
N3dal <nedalxzo@gmail.com>
2022-07-10 14:04:31 +0200

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,28 @@
+/*
+
+
+
+
+
+
+*/
+
+
+
+
 #include <iostream>
 #include <ctime>
 #include <random>
+#include "tools.h"
 
 using namespace std;
 
 
 int main(void){
 
+    // wipe terminal at first.
 
+    clear();
 
     return 0;
 }
