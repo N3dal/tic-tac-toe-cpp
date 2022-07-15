56017c4ac24679cf1a7a6b62bee48bd2f5c36682
N3dal <nedalxzo@gmail.com>
2022-07-15 12:06:25 +0200

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ TODO: ADD colors;
 #include <random>
 #include "tools.h"
 #include "game.h"
+#include <stdlib.h>
 
 using namespace std;
 
@@ -25,27 +26,7 @@ int main(void){
     clear();
 
 
-    // // now get the usr choice.
-    // int usr_choice = -1;
-
-    // while (usr_choice!=1 && usr_choice!=2){
-
-    //     // first print main-menu;
-    //     main_menu();
-    //     usr_choice = get_usr_choice();
-    // }
-
-    // if (usr_choice==1){
-    //     // Start New game.
-    // }
-
-    // else if (usr_choice==2){
-    //     // End the program (exit);
-    //     return 3;
-    // }
-    // else{
-    //     // in case we get -1 that's mean there's wrong.
-    // }
+    main_menu();
 
 
 
